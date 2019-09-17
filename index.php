5417c1c81586e10215d8a8395a076116c30fd8ba
Marcus Renato <marcusrenato@live.com>
2019-09-17 10:34:01 -0300

    Adicionando conexao ao banco de dados

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+try {
+    $pdo = new PDO("mysql:dbname=projeto_filtroemtabela;host=localhost", "root", "");
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
+?>
+
 <table border="1" width="100%">
     <tr>
         <th>Nome</th>
